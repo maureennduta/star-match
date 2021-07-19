7a28082889cdfa654d3cde412f8005a070c56607
Maureen Nduta <67265839+maureennduta@users.noreply.github.com>
2021-07-19 15:06:42 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom'; //use this to render
+//import { render, screen } from '@testing-library/react';
+import StarsDisplay  from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("renders without crashing", ()=> {
+    const div = document.createElement("div");
+    ReactDOM.render(<StarsDisplay />,div);
+    ReactDOM.unmountComponentAtNode(div);
+})
+//passed
